ff2c91fca8072e1bf9e924a6fe0464b9b239950d
elena-zavadskaya <130773080+elena-zavadskaya@users.noreply.github.com>
2023-08-02 21:20:02 +0300

    Добавила условия на проверку корректности ввода. Исправила выход из программы. Вынесла методы для взаимодействия с пользователем в отдельный класс. Добавила пояснения к формату ввода.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,68 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int numberOfPeople = splitTheBill();
-        requestProductInformation(calculator, numberOfPeople);
-    }
-
-    public static int splitTheBill() {
-        Scanner scanner = new Scanner(System.in);
-        int numberOfPeople = 0;
-        System.out.println("На скольких человек необходимо разделить счёт?");
-
-        if (scanner.hasNextInt()) {
-            numberOfPeople = scanner.nextInt();
-        } else {
-            System.out.println("Вы ввели что-то не то. Давайте попробуем снова.");
-            splitTheBill();
-        }
-
-        if (numberOfPeople == 1) {
-            System.out.println("В этом случае нет смысла ничего считать и делить. Давайте попробуем снова.");
-            splitTheBill();
-        } else if (numberOfPeople < 1) {
-            System.out.println("Это некорректное значение для подсчёта. Давайте попробуем снова.");
-            splitTheBill();
-        }
-        return numberOfPeople;
-    }
-
-    public static void requestProductInformation(Calculator calculator, int numberOfPeople) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Введите название товара.");
-        String name = null;
-        if (scanner.hasNextLine()) {
-            name = scanner.nextLine();
-        } else {
-            System.out.println("Вы ввели что-то не то. Давайте попробуем снова.");
-            requestProductInformation(calculator, numberOfPeople);
-        }
-
-        System.out.println("Введите стоимость товара.");
-        double cost = 0;
-
-        if (scanner.hasNextDouble()) {
-            cost = scanner.nextDouble();
-        } else {
-            System.out.println("Вы ввели что-то не то. Давайте попробуем снова.");
-            requestProductInformation(calculator, numberOfPeople);
-        }
-
-        calculator.addNewProduct(name, cost);
-        calculator.count(numberOfPeople);
-
-        System.out.println("Товар успешно добавлен.");
-        System.out.println("Хотите добавить еще один товар?");
-
-        String checking = scanner.next();
-
-        if (!checking.equalsIgnoreCase("Завершить")) {
-            requestProductInformation(calculator, numberOfPeople);
-        } else {
-            calculator.printList();
-        }
+        int numberOfPeople = UserInterface.splitTheBill();
+        UserInterface.requestProductInformation(calculator, numberOfPeople);
     }
 }
